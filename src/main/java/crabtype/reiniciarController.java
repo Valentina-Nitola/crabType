104b3966bf059f16c0c78dd22ddf7ec978544713
Valentina Nitola <valentina.nitola@correounivalle.edu.co>
2025-03-14 12:50:06 -0500

    Se añade un nuevo fxml con la pantalla para reiniciar el juego y volver a intentarlo junto a su controller

--- src/main/java/crabtype/reiniciarController.java
+++ src/main/java/crabtype/reiniciarController.java
@@ -1,4 +1,15 @@
 package crabtype;
 
-public class reiniciarController {
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+public class Controller {
+    @FXML private Button btnReiniciar;
+
+    btnReiniciar.setOnAction(event -> reiniciar());
+
+    private void reiniciar() {
+
+    }
 }
